1b829f70d057e3e09d36ab33cbb0e4b393a77a38
TomJaycocks <thomasjaycocks@gmail.com>
2021-04-14 22:13:17 +0100

    Initial Setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <global-header />
+  <router-view />
+  <global-footer />
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+  export default {
+    components:{
+      'global-header':Header,
+      'global-footer':Footer
+    }
+  }
+</script>
